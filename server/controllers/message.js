c2d672ffefe2d2ebc95b207ba8f0d649fd428cef
vishal <jakharvishalv@gmail.com>
2024-09-15 09:28:09 +0530

    chat socket error res

--- server/controllers/message.js
+++ server/controllers/message.js
@@ -1,30 +1,33 @@
 import Message from '../models/message.js';
-
 import io from '../server.js';
 
 const sendMessage = async (req, res) => {
     const { content, workspace } = req.body;
-    console.log(content, workspace);
-    const sender = req.user.id;
+    const senderId = req.user.id;
+
     try {
-        const message = new Message({ content, sender, workspace });
-        await message.save();
+        let message = new Message({ content, sender: senderId, workspace });
+        message = await message.save();
+
+        message = await message.populate('sender', 'name');
+
         io.to(workspace).emit('message', message);
         res.status(201).json({ message });
     } catch (error) {
+        console.error('Error sending message:', error);
         res.status(500).json({ message: 'Error sending message' });
     }
 };
 
 
+
 const getMessages = async (req, res) => {
     const { workspace } = req.query;
     try {
-        const messages = await Message.find({ workspace })
-            .populate('sender')
-            .sort({ timestamp: 1 });
+        const messages = await Message.find({ workspace }).populate('sender', 'name');
         res.status(200).json({ messages });
     } catch (error) {
+        console.error('Error fetching messages:', error);
         res.status(500).json({ message: 'Error fetching messages' });
     }
 };
